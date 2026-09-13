7481d32de2e1dc0838291e4baea75f90070d4023
Praveen Horatti <praveenhoratti2@gmail.com>
2026-09-13 23:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 108 MB (93.79%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int mn = arrays[0][0];
+        int mx = arrays[0].back();
+        int ans = 0;
+
+        for(int i = 1; i < arrays.size(); i++)
+        {
+            ans = max(ans, arrays[i].back() - mn);
+            ans = max(ans, mx - arrays[i][0]);
+
+            mn = min(mn, arrays[i][0]);
+            mx = max(mx, arrays[i].back());
+        }
+
+        return ans;
+    }
+};
