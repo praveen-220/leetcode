7ca0a47fb33db712c8a0ddeaf3a1cf20dca9e0ee
Praveen Horatti <praveenhoratti2@gmail.com>
2026-09-24 07:01:03 +0530

    Time: 3 ms (67.37%) | Memory: 16.1 MB (23.74%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(string current, int open, int close, int n) {
+        // Base case
+        if (current.size() == 2 * n) {
+            ans.push_back(current);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            solve(current + "(", open + 1, close, n);
+        }
+
+        // Add ')'
+        if (close < open) {
+            solve(current + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        solve("", 0, 0, n);
+        return ans;
+    }
+};
